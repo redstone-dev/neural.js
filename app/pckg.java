8eb57a0e8ca7b7663b6e6c81c11467da8538457e
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-11-03 13:35:41 -0400

    Update pckg.java

--- app/pckg.java
+++ app/pckg.java
@@ -2,6 +2,7 @@ package neuralJS
 
 class logs{
   public static void main(String[] args){
-    
+    System.out.println("Neural.JS: started")
+    System.warn.println("Neural.JS might take too long")
   }
 }
